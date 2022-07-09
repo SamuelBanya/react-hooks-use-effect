dd3ee3495f28edfe45aed53a5ecc998acd089edc
Samuel Banya <sbanya@fastmail.com>
2022-07-09 01:09:47 -0400

    Adding latest changes to assignment

--- src/App.js
+++ src/App.js
@@ -1,13 +1,42 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  // Example of using 'useEffect' for changing the 'title' of a page:
+  // This updates the page's title, but only when the 'text' variable changes:
+  useEffect(() => {
+    document.title = text;
+  }, [text]);
+
+  // Example of using 'useEffect' to create a timeout with 'setTimeout()' function:
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
+
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <br />
+      <DogPics />
+    </div>
+  );
 }
 
 export default App;
